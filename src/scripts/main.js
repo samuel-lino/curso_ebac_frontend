7eb57bf5c25bf5ef76c0a567ed765bfe68d006a9
samuel <samuellino2006@gmail.com>
2024-10-17 16:30:07 -0300

    tarefa finalizada

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const buttons =document.querySelectorAll('.product-button');
-
-    buttons.forEach(button=>{
-        button.addEventListener('click', function(e){
-            alert('Sobre o produto: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti esse molestiae quibusdam, corporis velit adipisci accusamus laborum, earum sed id praesentium eum sint ipsam dolorem nemo tempore repellat saepe cumque?')
-        });
-    })
-});
