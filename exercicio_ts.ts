e3792ce6ab98bdb1a5f78991d41c3f71dd00b34f
samuel <samuellino2006@gmail.com>
2025-05-26 17:42:20 -0300

    exercicio finalizado

---
+++ exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplicar(num1: number, num2: number): number{
+    return num1 * num2
+}
+
+function apresentar(nome: string): string{
+    return `Olá ${nome}`
+}
+
+multiplicar(3, 6)
+apresentar("João")
